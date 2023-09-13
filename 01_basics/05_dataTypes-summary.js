4e402706617566a0401fd31b4d0c311e2130a388
mishrameenu774 <77068936+mishrameenu774@users.noreply.github.com>
2023-09-13 16:29:43 +0000

    dataType summary

---
+++ 01_basics/05_dataTypes-summary.js
@@ -0,0 +1,35 @@
+// kis tarike se data ko memory mai store kiya jaata hai aur access kiya jaata hai is basis par data ko 2 parts mai divide kiya gya hai.
+
+// JavaScript is a Dynamic language
+
+//  Primitive
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+//Example of Symbol Data Type
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id);
+console.log(anotherId);
+console.log(id === anotherId); 
+
+// representation of BigInt
+const bigNumber = 3456543576654356754n
+
+
+//Reference (Non Primitive)
+
+// Arrays, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "Meenu",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber)
+
