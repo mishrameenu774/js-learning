9e85f003aa3a1e7dfb42b77e4b152f422a050ce5
mishrameenu774 <77068936+mishrameenu774@users.noreply.github.com>
2023-09-13 14:38:24 +0000

    conversion Data Types

--- 01_basics/03_conversionOperator.js
+++ 01_basics/03_conversionOperator.js
@@ -1,3 +1,5 @@
+//number conversion
+
 // let score = 33
 // // let score1 = "33abc"
 
@@ -11,7 +13,7 @@
 
 // console.log(typeof score1)
 
-// //conversion
+
 // let valueInNumber = Number(score)
 
 // console.log(typeof valueInNumber);
@@ -29,12 +31,48 @@
 //agar undefined ko number mai convert krna chahte hai tho output mai NaN milega (undefined) => NaN
 
 
-
-let isLoggedIn = null
+//boolean conversion
+/*let isLoggedIn = undefined
 
 let booleanIsLoogedIn = Boolean(isLoggedIn)
 console.log(typeof booleanIsLoogedIn)
-console.log(booleanIsLoogedIn)
+console.log(booleanIsLoogedIn)*/
 
 // agar kisi number ko string ko boolean mai convert krte hai tho result mai true milega 1=> true ; "meenu" => true
 // agar empty string ko boolean mai convert krte hai tho output mai false milega ("") => false
+// agar null ko boolean mai convert krte hai tho output mai false milega (null) => false
+// agar undefined ko boolean mai convert krte hai tho output mai false milega (undefined) => false
+
+
+
+//string conversion
+/* let str1 = "hello"
+let str2 = " Meenu"
+
+let str3 = str1+str2
+console.log(str3)
+
+/*agar hum conversion k time par string pehle likhte hai tab saari values string mai convert hoti hai 
+ whi agar number pehle likhte hai tab values ko number ko convert krti hai*/
+/*console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2) //ismai humne pehle string likha hai tho saari values string ki trh treat hogi tho output milega 122
+console.log(1+2+"2") //ismai humne pehle number likha hai tho number ki trh treat hua hai tho output milega 32
+
+console.log( (3 + 4) * 5 % 3)*/
+
+//not good practice don't do that
+// console.log(true)
+// console.log(+true)
+// console.log(+"") 
+
+// this code is correct but we can't use it as a coorporate level
+/*let num1, num2, num3
+num1 = num2 = num3 = 2+2
+console.log(num1)
+console.log(num2)
+console.log(num3)*/
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
