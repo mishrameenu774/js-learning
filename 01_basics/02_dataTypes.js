9e5dda80739b9c7a71c3e9ad577b2a22e909ae63
mishrameenu774 <77068936+mishrameenu774@users.noreply.github.com>
2023-09-13 00:34:46 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //it is used when we want to treat all JS code as newer version
+
+// alert(3+3) //We can't use it because we are not using browser we are working into node js
+
+console.log(3
+    +
+    3) //not to  write code like this because code readability should be high
+
+console.log("Meenu") 
+
+// Number => range 2 to power 53
+// bigInt => It is use when number is too high
+// boolean => true/false
+// string => ""
+// undefined => when value is not defined
+// null => it is standalone value and representation of empty value
+// symbol => to find uniqueness 
+
+//object
+
+console.log(typeof "Meenu")
+console.log(typeof null); // lekin iska type object hota hai
+console.log(typeof undefined); //type undefined hai
