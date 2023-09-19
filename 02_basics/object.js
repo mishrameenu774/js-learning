9932d14ec894b64c593214b672c1052e55bb6cd8
mishrameenu774 <77068936+mishrameenu774@users.noreply.github.com>
2023-09-19 17:07:12 +0000

    objects

---
+++ 02_basics/object.js
@@ -0,0 +1,7 @@
+//Singleton
+//agar hum literals k through object create krte hai tho singleton nhi hota vhi agar consturctor k through create krte hai tho singleton hota hai
+
+
+//object literals
+
+const JsUser = {}   //this line means we created empty object.
