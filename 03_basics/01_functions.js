ce2e931816c7336555802416363f40c140cb3f3b
mishrameenu774 <77068936+mishrameenu774@users.noreply.github.com>
2023-09-20 18:22:59 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,19 @@
+function sayMyName() {
+    console.log("M");
+    console.log("E");
+    console.log("E");
+    console.log("N");
+    console.log("U");
+}
+
+//function call
+sayMyName()
+
+function addTwoNmubers(number1, number2) {
+    console.log(number1 + number2)
+}
+
+addTwoNmubers(3, 4)
+addTwoNmubers(3, "4")
+addTwoNmubers(3, "a")
+addTwoNmubers(3, null)
